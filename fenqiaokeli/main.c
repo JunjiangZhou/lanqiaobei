a8531ec66c5726a8105c05f63e6e0d7359b05828
zhoujunjiang <hchen5273@gmail.com>
2023-11-06 20:12:37 +0800

    final ac

--- fenqiaokeli/main.c
+++ fenqiaokeli/main.c
@@ -7,29 +7,40 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    int n,k;
-    scanf("%d %d",&n,&k);
-    int c[n][2];
-    for (int i=0; i<n; i++) {
-        scanf("%d %d",&c[i][0],&c[i][1]);
+int main() {
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    int chocolates[N][2];
+    int maxSide = 0;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &chocolates[i][0], &chocolates[i][1]);
+        // 计算最大的边长上界
+        maxSide = (chocolates[i][0] > maxSide) ? chocolates[i][0] : maxSide;
+        maxSide = (chocolates[i][1] > maxSide) ? chocolates[i][1] : maxSide;
     }
-    int min=c[0][0];
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<2; j++) {
-            if (c[i][j]<min) {
-                min=c[i][j];
-            }
+
+    int left = 1;
+    int right = maxSide;
+    int result = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            count += (chocolates[i][0] / mid) * (chocolates[i][1] / mid);
+        }
+
+        if (count >= K) {
+            result = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    int result=0;
-    while (result<k&&min>0) {
-        result=0;
-        for (int i=0; i<n; i++) {
-            result+=(c[i][0]/min)*(c[i][1]/min);
-        }
-        if(result<k) min-=1;
-    }
-    printf("%d\n",min);
+
+    printf("%d\n", result);
     return 0;
 }
