db11cb9d243dfb4f5cd6c2d86e32651aea6f6ce6
zhoujunjiang <hchen5273@gmail.com>
2023-11-06 17:00:27 +0800

    1

--- fenqiaokeli/main.c
+++ fenqiaokeli/main.c
@@ -8,7 +8,27 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int c[n][2];
+    for (int i=0; i<n; i++) {
+        scanf("%d %d",&c[i][0],&c[i][1]);
+    }
+    int min=c[0][0];
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<2; j++) {
+            if (c[i][j]<min) {
+                min=c[i][j];
+            }
+        }
+    }
+    int result=0;
+    while (result<k&&min>0) {
+        for (int i=0; i<n; i++) {
+            result+=c[i][0]/min+c[i][1]/min;
+        }
+        min-=1;
+    }
+    printf("%d\n",min);
     return 0;
 }
